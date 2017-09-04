ecb486782407a184724d49a89f93f43e09f1795f
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of JDC variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosp/soong/android"
+	"jdc/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -98,7 +98,7 @@ type variableProperties struct {
 		}
 
 		// include JDC variables
-		*android.Product_variables
+		JDC android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -174,7 +174,7 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	// include JDC variables
-	*android.ProductVariables
+	JDC android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
